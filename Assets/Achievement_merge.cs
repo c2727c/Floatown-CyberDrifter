96f99ff6ac58d5ca72eaca2938067d9785820200
whostheme <33551850+whostheme@users.noreply.github.com>
2019-07-23 13:01:10 +0800

    --

--- Assets/Achievement_merge.cs
+++ Assets/Achievement_merge.cs
@@ -23,32 +23,34 @@ public class Achievement_merge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isCountingtime)
-        {
-            end_time = DateTime.Now;
-            ts = end_time - start_time;
+        // if(isCountingtime)
+        // {
+        //     end_time = DateTime.Now;
+        //     ts = end_time - start_time;
 
-            if(ts.Seconds == 5 )
-            {
-                notice.SetActive(false);
-                achievement_trigger.SetActive(false);
-            }
-        }
+        //     if(ts.Seconds == 5 )
+        //     {
+        //         notice.SetActive(false);
+        //         achievement_trigger.SetActive(false);
+        //     }
+        // }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(!isCountingtime)
-        {
-            Debug.Log("PreJug");
-            if(other.gameObject.CompareTag("Sword"))
-            {
-                Debug.Log("Trigger");
-                isCountingtime = true;
-                start_time = DateTime.Now;
-                end_time = DateTime.Now;
-                notice.SetActive(true);
-            }
-        }
+        // if(!isCountingtime)
+        // {
+        //     Debug.Log("PreJug");
+        //     if(other.gameObject.CompareTag("Sword"))
+        //     {
+        //         Debug.Log("Trigger");
+        //         isCountingtime = true;
+        //         start_time = DateTime.Now;
+        //         end_time = DateTime.Now;
+        //         notice.SetActive(true);
+        //     }
+        // }
+        
+        //画布的出现
     }
 }
